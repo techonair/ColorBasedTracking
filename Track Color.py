4bc30ebb427e2703e84bc0b80e3d3de960071436
Bhanu Pratap <techieonmail@gmail.com>
2021-09-26 22:13:25 +0530

    parameters updated

--- Track Color.py
+++ Track Color.py
@@ -7,12 +7,12 @@ cam = cv2.VideoCapture(0)
 
 while True:
     grabbed, img = cam.read()
-    img = cv2.resize(img, (21,21), fx= 1, fy= 1)
-    gaussianBlur = cv2.GaussianBlur(img, (21,21), 0.5)
+    img = cv2.resize(img, None, fx= 1, fy= 1)
+    gaussianBlur = cv2.GaussianBlur(img, (21,21), 0)
     hsvImg = cv2.cvtColor(gaussianBlur, cv2.COLOR_BGR2HSV)
 
-    hsvImg = cv2.erode(img, (21,21), iterations=1)
-    hsvImg = cv2.dilate(img, (21,21), iterations=2)
+    hsvImg = cv2.erode(img, None)[2]
+    hsvImg = cv2.dilate(img, None, iterations=2)
 
     cv2.imshow('Colored Object Tracking', img)
 
